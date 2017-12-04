2530c86ebae2795bea99e46dcc4a7df1ddabfc10
Shincero <mp2772@gmail.com>
2017-12-04 14:54:48 -0500

    *ngFor, index example

---
+++ angular-directives/src/app/animal-list/animals.ts
@@ -0,0 +1,32 @@
+export const animalList: any[] = [
+  {
+    id: 1,
+    name: "Dinosaur",
+    imageUrl: "https://media.giphy.com/media/oKltgjUfQSen6/giphy.gif"
+  },
+  {
+    id: 5,
+    name: "Platypus",
+    imageUrl: "https://media.giphy.com/media/FNpAjwtN1p1Yc/giphy.gif"
+  },
+  {
+    id: 15,
+    name: "Blue-footed Booby",
+    imageUrl: "https://media.giphy.com/media/1126MR3W3W5xyE/giphy.gif"
+  },
+  {
+    id: 20,
+    name: "Sloth",
+    imageUrl: "https://media.giphy.com/media/OocLVILtd7ybm/giphy.gif"
+  },
+  {
+    id: 25,
+    name: "Proboscis Monkey",
+    imageUrl: "https://media.giphy.com/media/XSfelJAzvBqYE/giphy.gif"
+  },
+  {
+    id: 30,
+    name: "Liger",
+    imageUrl: "https://media.giphy.com/media/nFKcj7I3MBAis/giphy.gif"
+  },
+];
