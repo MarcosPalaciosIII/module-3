1659f079e3c0825b7a4ac3f1d3e66027a4a00444
Shincero <mp2772@gmail.com>
2017-12-04 16:02:18 -0500

    ngStyle practice

--- angular-directives/src/app/app.component.ts
+++ angular-directives/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular Directives';
+
+  textColor: string = 'indigo';
+
+  ngOnInit() {
+    setInterval(
+      () => {
+        this.textColor = this.getRandomColor();
+      },
+      2200
+    );
+  }
+
+  getRandomColor() {
+
+    // random integers in the 0 - 255 range for rgb
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+
+    return `rgb(${r}, ${g}, ${b})`;
+  }
 }
