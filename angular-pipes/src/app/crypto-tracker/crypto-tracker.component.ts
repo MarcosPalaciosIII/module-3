7bd88b236e53647c8bcccb37bbce46334291570c
Shincero <mp2772@gmail.com>
2017-12-04 11:28:57 -0500

    angular pipes practice

---
+++ angular-pipes/src/app/crypto-tracker/crypto-tracker.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crypto-tracker',
+  templateUrl: './crypto-tracker.component.html',
+  styleUrls: ['./crypto-tracker.component.css']
+})
+export class CryptoTrackerComponent implements OnInit {
+
+  currentFartcoinPrice: number = 11445;
+
+  constructor() { }
+  //"ngOnInit()" runs when the component is initialized
+  //use it for extra setup needs to happen when the component loads
+  ngOnInit() {
+    setInterval(
+      () => {
+        // increase by 2.3%
+        this.currentFartcoinPrice *= 1.023;
+      },
+      2000
+    );
+  }
+
+}
