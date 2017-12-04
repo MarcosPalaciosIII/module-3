dc11c689b08c3ec76e84746c95e5e14bc3d05085
Shincero <mp2772@gmail.com>
2017-12-04 14:31:33 -0500

    *ngIf practice

---
+++ angular-directives/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { NumberInputComponent } from './number-input/number-input.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NumberInputComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
