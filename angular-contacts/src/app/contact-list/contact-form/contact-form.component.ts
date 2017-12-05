a542f56da44206c549ef4d878bb29b68ad886e24
Shincero <mp2772@gmail.com>
2017-12-05 17:48:49 -0500

    angular services practice

---
+++ angular-contacts/src/app/contact-list/contact-form/contact-form.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ContactService } from '../../services/contact.service'
+
+@Component({
+  selector: 'app-contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.css']
+})
+export class ContactFormComponent implements OnInit {
+
+  theContact: any = {};
+
+  constructor(private contactThang: ContactService) { }
+
+  ngOnInit() {
+  }
+
+  contactSubmit() {
+    this.contactThang.addContact(this.theContact);
+    this.theContact = {};
+  }
+
+}
